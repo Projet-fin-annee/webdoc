34356ed435ff4dfc52e48a3fe29648547588bdc6
Nstampfli <nathan.stampfli@hetic.net>
2020-06-07 18:18:39 +0200

    add home page all content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,22 @@
   <div class="home">
     <VideoHome></VideoHome>
     <Summary></Summary>
-    <Buttons></Buttons>
+    <div class="home__container">
+      <div class="home__titles">
+        <h1 class="home__mainTitle">“CAN WE <span>BREATHE</span> NOW ?”</h1>
+        <h3 class="home__subTitle">#BLACKLIVESMATTER</h3>
+      </div>
+      <div class="home__userContainer">
+        <Buttons></Buttons>
+        <div class="home__userExperience">
+          <p class="home__userExperienceTitle">POUR UNE MEILLEURE EXPERIENCE</p>
+          <div class="home__userExperienceIcons">
+            <img class="home__userExperienceIcon" src="../assets/icons/desktop.svg" alt="desktopIcons">
+            <img class="home__userExperienceIcon" src="../assets/icons/cask.svg" alt="caskIcons">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -39,6 +54,67 @@ export default {
   font-size: 14px;
   @include medium {
     font-size: 16px;
+  }
+  &__container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    justify-content: space-between;
+  }
+  &__titles{
+    font-weight: bold;
+    text-align: center;
+    margin-top: 100px;
+  }
+  &__mainTitle{
+    font-size: 22px;
+    & span{
+      color: $primary-color;
+    }
+    @include medium {
+      font-size: 42px;
+    }
+    @include large {
+      font-size: 60px;
+    }
+  }
+  &__subTitle{
+    font-size: 16px;
+    color: $light-grey;
+    @include medium {
+      font-size: 20px;
+    }
+    @include large {
+      font-size: 24px;
+    }
+  }
+  &__userContainer{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+  &__userExperience{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 80px 0px;
+
+  }
+  &__userExperienceIcons{
+    display: flex;
+    justify-content: space-evenly;
+    width: 80%;
+    margin-top: 16px;
+  }
+  &__userExperienceIcon{
+    width: 20px;
+    @include medium {
+      width: 26px;
+    }
+    @include large {
+      width: 32px;
+    }
   }
 }
 </style>
