b7bf07abc45150a7c29469606396b493ff6a0fb4
Maeva Wolff <maeva.wolff@hetic.net>
2020-06-08 20:50:33 +0200

    add sound button on app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,22 @@
     <vue-page-transition name="overlay-right-full">
       <router-view />
     </vue-page-transition>
+    <SoundButton></SoundButton>
   </div>
 </template>
 
 <script>
+import SoundButton from "@/components/SoundButton.vue";
+
 export default {
-  components: {}
+  components: {
+    SoundButton
+  }
 };
 </script>
 
 <style lang="scss">
-@import './scss/style.scss';
+@import "./scss/style.scss";
 
 #app {
   font-family: Arial, sans-serif;
