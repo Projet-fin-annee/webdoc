6fc83bf20d1e76f90b8797c9ca4ea2b92bcabbc1
Maeva Wolff <maeva.wolff@hetic.net>
2020-06-13 17:22:57 +0200

    add background in blm page

--- src/views/movements/BlackLivesMatter.vue
+++ src/views/movements/BlackLivesMatter.vue
@@ -1,21 +1,26 @@
-<template class="blackLivesMatter">
-  <div>
+<template>
+  <div class="blackLivesMatter">
     <TextStroke type="whiteStroke">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis officia
-      porro mollitia eligendi <span class="pink">quos</span>ipsam molestias
+      porro mollitia eligendi
+      <span
+        class="pink"
+      >quos</span>ipsam molestias
       illum animi earum omnis qui voluptates culpa incidunt placeat
-      <span class="white">beatae ea, laudantium nobis</span>
+      <span
+        class="white"
+      >beatae ea, laudantium nobis</span>
       tempore!
     </TextStroke>
-    <TextStroke type="pinkStroke"
-      >Lorem ipsum dolor sit amet consectetur, adipisicing
-      <span class="pink">elit.</span>Quaerat nobis maxime</TextStroke
-    >
+    <TextStroke type="pinkStroke">
+      Lorem ipsum dolor sit amet consectetur, adipisicing
+      <span class="pink">elit.</span>Quaerat nobis maxime
+    </TextStroke>
   </div>
 </template>
 
 <script>
-import TextStroke from '@/components/TextStroke.vue';
+import TextStroke from "@/components/TextStroke.vue";
 
 export default {
   components: {
@@ -27,6 +32,7 @@ export default {
 <style lang="scss">
 .blackLivesMatter {
   @include backgroundCenter;
-  background-image: url('../../assets/background2.png');
+  background-image: url("../../assets/background2.png");
 }
 </style>
+  
