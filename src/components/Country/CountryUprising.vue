2c65b27a47f446ae9210d56480ee2296ef278ac0
Maeva Wolff <maeva.wolff@hetic.net>
2020-06-13 21:38:31 +0200

    add uprising component for country page

--- src/components/Country/CountryUprising.vue
+++ src/components/Country/CountryUprising.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="countryUprising"></div>
+  <div class="countryUprising">
+    <h2 class="countryUprising__title">“QUI NOUS PROTège de ceux censés nous protéger ?”</h2>
+    <div class="countryUprising__details">
+      <div class="countryUprising__images">
+        <img
+          src="../../assets/country/uprising_us1.png"
+          alt="image uprising"
+          class="countryUprising__image1"
+        />
+        <img
+          src="../../assets/country/uprising_us2.png"
+          alt="image uprising"
+          class="countryUprising__image2"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,5 +26,14 @@ export default {};
 .countryUprising {
   min-height: 100vh;
   padding-top: 110px;
+
+  &__title {
+    color: $primary-color;
+    text-transform: uppercase;
+    font-weight: 900;
+    @include large {
+      font-size: 50px;
+    }
+  }
 }
 </style>
