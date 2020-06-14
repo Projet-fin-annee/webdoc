0d3a13d2f80fd2b0fa18ac29fd22fa66716e71cd
Nstampfli <nathan.stampfli@hetic.net>
2020-06-14 21:07:06 +0200

    add animation and blackpanter is finish

--- src/views/movements/BlackPanthers.vue
+++ src/views/movements/BlackPanthers.vue
@@ -10,31 +10,41 @@
       >
     </TimelineInfo>
     </TimelineBar>
+    <transition name="fade">
     <TextStroke type="whiteStroke" v-if="currentInfo == 1">
      CRéations du mouvement suite a la<span class="white">mort de malcom x militantiste noir américain</span>
      leurs but de <span class="white">combattre les injustices</span>
       que subissent les  afro-AMéricainS AVEC UN programme en dix points a appelé à la cessation immédiate des
       <span class="white">brutalités policières</span>; emploi pour les Afro-Américains; et la terre, le logement et la justice pour tous. 
     </TextStroke>
+    </transition>
+    <transition name="fade">
     <TextStroke type="pinkStroke" v-if="currentInfo == 2">
       Le FBI deploie des
       <span class="pink">mesures de répressions</span>Déstinées aux communistes contre l’ORGANISATION jugée
       <span class="pink">illégale</span>
     </TextStroke>
+    </transition>
+    <transition name="fade">
     <TextStroke type="whiteStroke" v-if="currentInfo == 3">
       <span class="white">+2000 membres des  BLACK PANTERS</span>
        a travers le pays le poing levé vers le ciel, est leurs signe de protestation
     </TextStroke>
+    </transition>
+    <transition name="fade">
     <TextStroke type="whiteStroke" v-if="currentInfo == 4">
       <span class="white">ATTAQUE POLICIère au siège des black panters </span>
        et se termine par la chute du siège suite a 4H D’AFFRONTEMENTS
     </TextStroke>
+    </transition>
+    <transition name="fade">
     <TextStroke type="pinkStroke" v-if="currentInfo == 5">
       22 août 1969
       <span class="pink">assassinnat</span>de
-      <span class="pink">HOEY P. NEWTON </span>
+      <span class="pink">HOEY P.NEWTON </span>
       co-fondateur des black panters, le mouvement s’éteind peu a peu
     </TextStroke>
+    </transition>
   </div>
 </template>
 
@@ -90,4 +100,11 @@ export default {
     overflow-y: hidden;
     background-attachment: fixed;
 }
+ .fade-enter-active {
+  transition: opacity .8s ease-in-out;
+}
+.fade-enter {
+  opacity: 0;
+}
+
 </style>
