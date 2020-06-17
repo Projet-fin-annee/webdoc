af6d7c5e94985cb05412f877a1560d5166fbaecc
tania-abitbol <tabitbol@hotmail.com>
2020-06-17 17:18:04 +0200

    oline test

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,10 @@
 export const getCountries = () =>
-  fetch("http://custom-4c36.frb.io/countries").then((resp) => {
+  fetch("https://custom-4c36.frb.io/countries").then((resp) => {
     console.log("resp", resp);
     return resp.json();
   });
 
 export const getCountry = (country) =>
-  fetch(`http://custom-4c36.frb.io/countries/${country}`).then((resp) => {
+  fetch(`https://custom-4c36.frb.io/countries/${country}`).then((resp) => {
     return resp.json();
   });
